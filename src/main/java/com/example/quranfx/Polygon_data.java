8c57a3f3ac03a245a2fee2bc45eb3f52219c76c2
Abdelrahman Abdelkader <abdulrahman.m.m600@hotmail.com>
2025-08-16 01:42:34 -0400

    Created an enum to better track the status of the polygon in relation to the time line

--- src/main/java/com/example/quranfx/Polygon_data.java
+++ src/main/java/com/example/quranfx/Polygon_data.java
@@ -3,10 +3,12 @@ package com.example.quranfx;
 public class Polygon_data {
     private double real_polygon_position;
     private double polygon_width;
+    private boolean should_the_polygon_be_fixed_in_the_middle;
 
     public Polygon_data(double real_polygon_position, double polygon_width) {
         this.real_polygon_position = real_polygon_position;
         this.polygon_width = polygon_width;
+        this.should_the_polygon_be_fixed_in_the_middle = false;
     }
 
     public double getReal_polygon_position() {
@@ -19,5 +21,13 @@ public class Polygon_data {
 
     public void setReal_polygon_position(double real_polygon_position) {
         this.real_polygon_position = real_polygon_position;
+    }
+
+    public boolean isShould_the_polygon_be_fixed_in_the_middle() {
+        return should_the_polygon_be_fixed_in_the_middle;
+    }
+
+    public void setShould_the_polygon_be_fixed_in_the_middle(boolean should_the_polygon_be_fixed_in_the_middle) {
+        this.should_the_polygon_be_fixed_in_the_middle = should_the_polygon_be_fixed_in_the_middle;
     }
 }
---
+++ src/main/java/com/example/quranfx/Polygon_mode_time_line.java
@@ -0,0 +1,7 @@
+package com.example.quranfx;
+
+public enum Polygon_mode_time_line {
+    BEFORE_MIDDLE,
+    MIDDLE,
+    AFTER_MIDDLE
+}
