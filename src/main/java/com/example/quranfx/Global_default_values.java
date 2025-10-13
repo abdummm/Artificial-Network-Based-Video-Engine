a02442585d3e516788325d5d17349636fee9d6a0
Abdelrahman Abdelkader <abdulrahman.m.m600@hotmail.com>
2025-10-13 10:23:51 -0400

    Added default canvas width.

--- src/main/java/com/example/quranfx/Global_default_values.java
+++ src/main/java/com/example/quranfx/Global_default_values.java
@@ -2,4 +2,5 @@ package com.example.quranfx;
 
 public class Global_default_values {
     public static final double stroke_weight = 1;
+    public static final double canvas_default_width = 1080;
 }
