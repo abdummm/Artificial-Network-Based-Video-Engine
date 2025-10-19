e0c4f67e515978b18089898f6d89d734026308ca
Abdelrahman Abdelkader <abdulrahman.m.m600@hotmail.com>
2025-10-19 12:17:57 -0400

    Added a constructor and a value that indicates the listener type.

--- src/main/java/com/example/quranfx/Listener_info.java
+++ src/main/java/com/example/quranfx/Listener_info.java
@@ -6,6 +6,13 @@ public class Listener_info {
     private ChangeListener<Number> change_listener;
     private boolean listener_set = false;
     private boolean listener_attached = false;
+    private Listener_type listener_type;
+
+    public Listener_info(boolean listener_set, boolean listener_attached, Listener_type listener_type) {
+        this.listener_set = listener_set;
+        this.listener_attached = listener_attached;
+        this.listener_type = listener_type;
+    }
 
     public ChangeListener<Number> getChange_listener() {
         return change_listener;
@@ -29,5 +36,13 @@ public class Listener_info {
 
     public void setListener_attached(boolean listener_attached) {
         this.listener_attached = listener_attached;
+    }
+
+    public Listener_type getListener_type() {
+        return listener_type;
+    }
+
+    public void setListener_type(Listener_type listener_type) {
+        this.listener_type = listener_type;
     }
 }
