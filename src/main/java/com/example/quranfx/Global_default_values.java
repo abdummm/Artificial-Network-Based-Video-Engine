e0d79ba403d1c1af6241da1c7f8d088f24c31785
Abdelrahman Abdelkader <abdulrahman.m.m600@hotmail.com>
2025-10-21 12:29:01 -0400

    Added a new default value.

--- src/main/java/com/example/quranfx/Global_default_values.java
+++ src/main/java/com/example/quranfx/Global_default_values.java
@@ -2,5 +2,5 @@ package com.example.quranfx;
 
 public class Global_default_values {
     public static final double stroke_weight = 1;
-    public static final double canvas_default_width = 1080;
+    public static final double max_fade_in_out_image_video = 2.0;
 }
