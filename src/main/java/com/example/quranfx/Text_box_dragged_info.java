683a03f39dce47ae147bbf6587d9c3b507522cd1
Abdelrahman Abdelkader <abdulrahman.m.m600@hotmail.com>
2025-10-25 13:41:07 -0400

    Added Text_box_dragged_info.java

---
+++ src/main/java/com/example/quranfx/Text_box_dragged_info.java
@@ -0,0 +1,29 @@
+package com.example.quranfx;
+
+public class Text_box_dragged_info {
+    private Text_box_info text_box_info;
+    private Language_info language_info;
+    private boolean set = false;
+
+    public Text_box_dragged_info() {
+        this.set = false;
+    }
+
+    public Text_box_dragged_info(Text_box_info text_box_info, Language_info language_info) {
+        this.text_box_info = text_box_info;
+        this.language_info = language_info;
+        this.set = true;
+    }
+
+    public Text_box_info getText_box_info() {
+        return text_box_info;
+    }
+
+    public Language_info getLanguage_info() {
+        return language_info;
+    }
+
+    public boolean isSet() {
+        return set;
+    }
+}
