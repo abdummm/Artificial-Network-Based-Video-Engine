4aa9a89d166380b663a8200514c7612e26fc44aa
Abdelrahman Abdelkader <abdulrahman.m.m600@hotmail.com>
2025-10-26 12:12:33 -0400

    Added a height and width variables to Text_on_canvas_dragged.java

--- src/main/java/com/example/quranfx/Text_on_canvas_dragged.java
+++ src/main/java/com/example/quranfx/Text_on_canvas_dragged.java
@@ -7,39 +7,45 @@ public class Text_on_canvas_dragged {
     private Point2D original_point2D_of_text;
     private Language_info language_info;
     private Type_of_cursor type_of_cursor;
-    private boolean data_set = false;
+    private Text_item text_item;
+    private double original_height;
+    private double original_width;
+
+    public Text_on_canvas_dragged(Point2D original_point2D_of_mouse_event, Point2D original_point2D_of_text, Language_info language_info, Type_of_cursor type_of_cursor, Text_item text_item, double original_height, double original_width) {
+        this.original_point2D_of_mouse_event = original_point2D_of_mouse_event;
+        this.original_point2D_of_text = original_point2D_of_text;
+        this.language_info = language_info;
+        this.type_of_cursor = type_of_cursor;
+        this.text_item = text_item;
+        this.original_height = original_height;
+        this.original_width = original_width;
+    }
 
     public Point2D getOriginal_point2D_of_mouse_event() {
         return original_point2D_of_mouse_event;
-    }
-
-    public void setOriginal_point2D_of_mouse_event(Point2D original_point2D_of_mouse_event) {
-        this.original_point2D_of_mouse_event = original_point2D_of_mouse_event;
     }
 
     public Language_info getLanguage_info() {
         return language_info;
     }
 
-    public void setLanguage_info(Language_info language_info) {
-        this.language_info = language_info;
-    }
-
-    public boolean isData_set() {
-        return data_set;
-    }
-
-    public void setData_set(boolean data_set) {
-        this.data_set = data_set;
-    }
-
     public Point2D getOriginal_point2D_of_text() {
         return original_point2D_of_text;
     }
 
-    public void setOriginal_point2D_of_text(Point2D original_point2D_of_text) {
-        this.original_point2D_of_text = original_point2D_of_text;
+    public Type_of_cursor getType_of_cursor() {
+        return type_of_cursor;
     }
 
+    public Text_item getText_item() {
+        return text_item;
+    }
 
+    public double getOriginal_height() {
+        return original_height;
+    }
+
+    public double getOriginal_width() {
+        return original_width;
+    }
 }
