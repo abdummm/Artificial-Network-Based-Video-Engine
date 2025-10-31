efb207bf80e4b420a59840a1f0cd2944ac051258
Abdelrahman Abdelkader <abdulrahman.m.m600@hotmail.com>
2025-10-31 15:20:37 -0400

    Added two values to Global_default_values.java

--- src/main/java/com/example/quranfx/Global_default_values.java
+++ src/main/java/com/example/quranfx/Global_default_values.java
@@ -3,4 +3,6 @@ package com.example.quranfx;
 public class Global_default_values {
     public static final double stroke_weight = 1;
     public static final double max_fade_in_out_image_video = 2.0;
+    private static final double extra_width_padding = 25D;
+    private static final double extra_height_padding = 20D;
 }
