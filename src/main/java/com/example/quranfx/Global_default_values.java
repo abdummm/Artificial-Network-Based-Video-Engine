91c5c721528ecdba42bf73cb403259c7c9820472
Abdelrahman Abdelkader <abdulrahman.m.m600@hotmail.com>
2025-11-05 16:48:02 -0500

    Add shadow_weight constant to Global_default_values for better control.

--- src/main/java/com/example/quranfx/Global_default_values.java
+++ src/main/java/com/example/quranfx/Global_default_values.java
@@ -2,5 +2,6 @@ package com.example.quranfx;
 
 public class Global_default_values {
     public static final double stroke_weight = 1;
+    public static final double shadow_weight = 7.5D;
     public static final double max_fade_in_out_image_video = 2.0;
 }
