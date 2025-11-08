3e532867ca64550928ffe03ad3f106164ef91c55
Abdelrahman Abdelkader <abdulrahman.m.m600@hotmail.com>
2025-11-07 23:15:59 -0500

    Add text_item field and update methods for dynamic sizing adjustments.

--- src/main/java/com/example/quranfx/Text_box_info.java
+++ src/main/java/com/example/quranfx/Text_box_info.java
@@ -16,6 +16,7 @@ public class Text_box_info {
     private boolean set = false;
     private double min_width;
     private double min_height;
+    private Text_item text_item;
 
     public Text_box_info(Text_item text_item, Point2D center_position, String adjusted_verse, Font font, boolean visible) {
         this.center_position = new Point2D(center_position.getX(), center_position.getY());
@@ -23,7 +24,7 @@ public class Text_box_info {
         //this.text_box_width = Math.min(width_and_height[0] + extra_width_padding, 1080);
         this.text_box_width = 1080;
         this.text_box_height = width_and_height[1] + text_item.getExtra_height_padding();
-        this.min_width = return_the_min_width(adjusted_verse,font) + text_item.getExtra_width_padding();
+        this.min_width = return_the_min_width(adjusted_verse, font) + text_item.getExtra_width_padding();
         this.min_height = width_and_height[1] + text_item.getExtra_height_padding();
         this.visible = visible;
         this.min_x_point = center_position.getX() - this.text_box_width / 2;
@@ -31,13 +32,15 @@ public class Text_box_info {
         this.min_y_point = center_position.getY() - this.text_box_height / 2;
         this.max_y_point = center_position.getY() + this.text_box_height / 2;
         this.set = true;
+        this.text_item = text_item;
     }
+
     private double return_the_min_width(String adjusted_verse_text, Font font) {
         String[] words = adjusted_verse_text.split(" ");
         Text text = new Text();
         text.setFont(font);
         double min_width = 0;
-        for(String word : words){
+        for (String word : words) {
             text.setText(word);
             min_width = Math.max(min_width, text.getLayoutBounds().getWidth());
         }
@@ -138,5 +141,15 @@ public class Text_box_info {
         this.min_height = min_height;
     }
 
-
+    public void update_the_min_height_and_width() {
+        min_width = return_the_min_width(text_item.getAdjusted_verse_text(), text_item.getFont()) + text_item.getExtra_width_padding();
+        text_box_width = Math.max(min_width, text_box_width);
+        text_box_width = Math.min(text_box_width,text_item.getVideo_width());
+        String new_adjusted_verse = Text_sizing.getInstance().do_i_need_to_resize_the_verse_text(text_item.getVerse_text(), text_item.getFont(), text_box_width - text_item.getExtra_width_padding(), text_item.getLeft_margin(), text_item.getRight_margin());
+        text_item.setAdjusted_verse_text(new_adjusted_verse);
+        min_height = Text_sizing.getInstance().get_width_and_height_of_string(text_item.getAdjusted_verse_text(), text_item.getFont())[1] + text_item.getExtra_height_padding();
+        text_box_height = /*Math.max(min_height, text_box_height);*/ min_height;
+        update_the_x_points();
+        update_the_y_points();
+    }
 }
