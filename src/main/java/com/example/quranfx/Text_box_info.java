2a8193c61c3b567c356cca2bfecc669a49dada9b
Abdelrahman Abdelkader <abdulrahman.m.m600@hotmail.com>
2025-11-12 23:23:52 -0500

    Refactor min width calculation to use Text_sizing instance method.

--- src/main/java/com/example/quranfx/Text_box_info.java
+++ src/main/java/com/example/quranfx/Text_box_info.java
@@ -24,7 +24,7 @@ public class Text_box_info {
         //this.text_box_width = Math.min(width_and_height[0] + extra_width_padding, 1080);
         this.text_box_width = 1080;
         this.text_box_height = width_and_height[1] + text_item.getExtra_height_padding();
-        this.min_width = return_the_min_width(adjusted_verse, font) + text_item.getExtra_width_padding();
+        this.min_width = Text_sizing.getInstance().return_the_min_width(adjusted_verse, font) + text_item.getExtra_width_padding();
         this.min_height = width_and_height[1] + text_item.getExtra_height_padding();
         this.visible = visible;
         this.min_x_point = center_position.getX() - this.text_box_width / 2;
@@ -33,18 +33,6 @@ public class Text_box_info {
         this.max_y_point = center_position.getY() + this.text_box_height / 2;
         this.set = true;
         this.text_item = text_item;
-    }
-
-    private double return_the_min_width(String adjusted_verse_text, Font font) {
-        String[] words = adjusted_verse_text.split(" ");
-        Text text = new Text();
-        text.setFont(font);
-        double min_width = 0;
-        for (String word : words) {
-            text.setText(word);
-            min_width = Math.max(min_width, text.getLayoutBounds().getWidth());
-        }
-        return min_width;
     }
 
     public double getText_box_width() {
@@ -142,7 +130,7 @@ public class Text_box_info {
     }
 
     public void update_the_min_height_and_width() {
-        min_width = return_the_min_width(text_item.getAdjusted_verse_text(), text_item.getFont()) + text_item.getExtra_width_padding();
+        min_width = Text_sizing.getInstance().return_the_min_width(text_item.getAdjusted_verse_text(), text_item.getFont()) + text_item.getExtra_width_padding();
         text_box_width = Math.max(min_width, text_box_width);
         text_box_width = Math.min(text_box_width,text_item.getVideo_width());
         String new_adjusted_verse = Text_sizing.getInstance().do_i_need_to_resize_the_verse_text(text_item.getVerse_text(), text_item.getFont(), text_box_width - text_item.getExtra_width_padding(), text_item.getLeft_margin(), text_item.getRight_margin());
