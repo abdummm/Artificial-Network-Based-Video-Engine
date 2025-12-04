5fb44e9f12d22862a5376d2028adfc9da9546f34
Abdelrahman Abdelkader <abdulrahman.m.m600@hotmail.com>
2025-12-04 18:29:05 -0500

    Add maximum stroke and shadow weight constants to global defaults.

--- src/main/java/com/example/quranfx/Global_default_values.java
+++ src/main/java/com/example/quranfx/Global_default_values.java
@@ -2,6 +2,8 @@ package com.example.quranfx;
 
 public class Global_default_values {
     public static final double stroke_weight = 1;
+    public static final double max_stroke_weight = 15;
     public static final double shadow_weight = 7.5D;
+    public static final double max_shadow_weight = 10;
     public static final double max_fade_in_out_image_video = 2.0;
 }
