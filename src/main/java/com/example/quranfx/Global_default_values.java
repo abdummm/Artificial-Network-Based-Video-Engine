83eb1eb17f91a1c7fb74fa9be295143bf77c5ca0
Abdelrahman Abdelkader <abdulrahman.m.m600@hotmail.com>
2025-12-19 12:06:48 -0500

    Update max_shadow_weight to 15 for improved shadow effects.

--- src/main/java/com/example/quranfx/Global_default_values.java
+++ src/main/java/com/example/quranfx/Global_default_values.java
@@ -4,7 +4,7 @@ public class Global_default_values {
     public static final double stroke_weight = 1;
     public static final double max_stroke_weight = 15;
     public static final double shadow_weight = 7.5D;
-    public static final double max_shadow_weight = 10;
+    public static final double max_shadow_weight = 15;
     public static final double max_fade_in_out_image_video = 2.0;
     public static final float height_text_margin = 10;
 }
