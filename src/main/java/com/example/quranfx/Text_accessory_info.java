c655e5179425a53d97e089c8c4f0df01efaac953
Abdelrahman Abdelkader <abdulrahman.m.m600@hotmail.com>
2026-02-16 00:12:11 -0500

    Refactor Text_accessory_info constructor to handle all parameters efficiently.

--- src/main/java/com/example/quranfx/Text_accessory_info.java
+++ src/main/java/com/example/quranfx/Text_accessory_info.java
@@ -3,14 +3,24 @@ package com.example.quranfx;
 import javafx.scene.paint.Color;
 
 public class Text_accessory_info {
+    private Accessory_type accessory_type;
     private boolean is_the_accessory_on;
     private Color accessory_color;
     private double accessory_weight;
     private double max_accessory_weight;
 
     public Text_accessory_info(Accessory_type accessory_type, double accessory_weight,double max_accessory_weight) {
+        this.accessory_type = accessory_type;
         this.is_the_accessory_on = false;
         this.accessory_color = Color.BLACK;
+        this.accessory_weight = accessory_weight;
+        this.max_accessory_weight = max_accessory_weight;
+    }
+
+    public Text_accessory_info(Accessory_type accessory_type, boolean is_the_accessory_on, Color accessory_color, double accessory_weight, double max_accessory_weight) {
+        this.accessory_type = accessory_type;
+        this.is_the_accessory_on = is_the_accessory_on;
+        this.accessory_color = accessory_color;
         this.accessory_weight = accessory_weight;
         this.max_accessory_weight = max_accessory_weight;
     }
@@ -45,5 +55,9 @@ public class Text_accessory_info {
 
     public void setMax_accessory_weight(double max_accessory_weight) {
         this.max_accessory_weight = max_accessory_weight;
+    }
+
+    public Accessory_type getAccessory_type() {
+        return accessory_type;
     }
 }
