2847cd939571ef74975ace6f5faccc3fe7641a61
Abdelrahman Abdelkader <abdulrahman.m.m600@hotmail.com>
2026-05-07 23:23:35 -0400

    Update module-info.java requirements for JavaFX libraries and dependencies.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,40 +1,33 @@
 module com.example.quranfx {
-    requires javafx.web;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
     requires javafx.media;
+    requires javafx.swing;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires com.almasb.fxgl.all;
     requires okhttp3;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
     requires org.jsoup;
-    requires javafx.swing;
+
     requires org.bytedeco.javacv;
     requires org.bytedeco.ffmpeg;
-    requires org.apache.commons.lang3;
+
     requires org.apache.commons.io;
     requires thumbnailator;
     requires metadata.extractor;
     requires imgscalr.lib;
     requires com.jfoenix;
-    requires org.bytedeco.opencv;
-    requires atlantafx.base;
-    requires MaterialFX;
-    requires jdk.compiler;
-    requires annotations;
-    requires javafx.base;
-    requires javafx.graphics;
-    requires org.checkerframework.checker.qual;
+
     requires io.github.humbleui.skija.shared;
-    requires io.github.humbleui.types;
     requires java.prefs;
     requires java.net.http;
     requires jave.core;
-
+    requires annotations;
+    requires java.desktop;
+    requires org.controlsfx.controls;
+    requires MaterialFX;
 
     opens com.example.quranfx to javafx.fxml;
     exports com.example.quranfx;
